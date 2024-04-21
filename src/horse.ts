cc144046167c6f5e8f2096070207e632d2dd7e02
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2024-04-21 19:01:05 +0900

    デコレータ、Raceの定義を追加

--- src/horse.ts
+++ src/horse.ts
@@ -5,6 +5,42 @@
 // また、型は固有の文字列を持つため、文字列リテラル型を使っている
 type Sex = "male" | "female" | "gelding"
 
+// 馬場
+// 芝・ダート・障害・AWに対応させている
+type groundType = "turf" | "dirt" | "hurdle" | "AW";
+
+// ログを出力するデコレータ
+function logged(originalMethod: any, context: any) {
+    return function loggedMethod(this: any, ...args: any[]) {
+        console.log(`${context.name}メソッドの呼び出し開始`);
+        const result = originalMethod.call(this, ...args);
+        console.log(`${context.name}メソッドの呼び出し終了`);
+        return result;
+    }
+}
+
+interface Race {
+    // レース名
+    readonly name: string;
+    // 日付
+    readonly date: Date;
+    // 場所
+    readonly place: string;
+    // 距離
+    readonly distance: number;
+    // 馬場
+    readonly ground: groundType;
+    // 着順
+    readonly rank: number;
+    // 馬体重
+    readonly weight: number;
+    // 騎手
+    readonly jockey: string;
+    // 騎手斤量
+    readonly jockeyWeight: number;
+
+}
+
 // 競走馬のクラス
 class Horse {
     // 名前
@@ -15,6 +51,10 @@ class Horse {
     // 性別
     // 性別を変更する際にはchangeSexToGeldingメソッドを使うため、ここではreadonlyをつけていない
     readonly sex: Sex;
+    // レース戦績
+    // レース戦績は配列で持つ
+    // 配列の要素はRace型である
+    raceResults?: Race[];
 
     constructor(
         name: string,
@@ -27,6 +67,7 @@ class Horse {
     }
 
     // 性別を去勢された馬に変更する
+    @logged
     changeSexToGelding(horse: Horse): Horse {
         // 性別がmale以外の場合はエラーを投げる
         if (horse.sex !== "male") {
@@ -38,6 +79,7 @@ class Horse {
 
     // 年齢を取得する
     // 1月1日に年齢が1つ上がる計算のため、年数だけを取得して計算する
+    @logged
     getAge(): number {
         // 現在の日付を取得
         // テストを書くためには本当はここに変化する日付を外から注入するようにした方が良い
@@ -47,6 +89,11 @@ class Horse {
         return now.getFullYear() - this.birth.getFullYear();
     }
 
+    // 馬の情報を取得する
+    @logged
+    getInfo(): string {
+        return `馬名: ${this.name}, 性別: ${this.sex}, 生年月日: ${this.birth.toLocaleDateString()}`;
+    }
 }
 
 const horse: Horse = new Horse("ディープインパクト", new Date("2002-03-25"), "male");
@@ -56,3 +103,5 @@ console.log(horse.getAge());
 console.log(horse.name);
 // 性別を取得する
 console.log(horse.sex);
+// 情報を取得する
+console.log(horse.getInfo());
